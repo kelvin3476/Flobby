44d2f337d51eb6202bd57655f9f844082509ed1e
kelvin <seungu.lee@g.austincc.edu>
2024-12-31 16:27:14 +0900

    chore: typescript 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
+import React from "react";
+import Login from "./pages/Login";
 import './App.css';
-import Login from "./components/Login.jsx";
 
 function App() {
   return (
     <>
-        <Login />
+      <Login />
     </>
   );
 }
