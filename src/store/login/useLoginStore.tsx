6abae13e9a43b02eb6d3e1555fad00258c08fd47
kelvin <seungu.lee@g.austincc.edu>
2025-01-02 16:38:08 +0900

    Feat: 로그인 페이지 상태관리 스토어 추가 (Zustand)

---
+++ src/store/login/useLoginStore.tsx
@@ -0,0 +1,49 @@
+import { create } from "zustand";
+
+interface LoginStore {
+    /* 이메일 상태 관리 */
+    email: string;
+    setEmail: (email: string) => void;
+
+    /* 이메일 유효성 검사 상태 관리 */
+    isEmailValid: boolean;
+    setIsEmailValid: (isEmailValid: boolean) => void;
+
+    /* 이메일 에러 상태 관리 */
+    emailError: string;
+    setEmailError: (emailError: string) => void;
+
+    /* 비밀번호 상태 관리 */
+    password: string;
+    setPassword: (password: string) => void;
+
+    /* 비밀번호 유효성 검사 상태 관리 */
+    isPasswordValid: boolean;
+    setIsPasswordValid: (isPasswordValid: boolean) => void;
+
+    /* 비밀번호 에러 상태 관리 */
+    passwordError: string;
+    setPasswordError: (passwordError: string) => void;
+}
+
+const useLoginStore = create<LoginStore>((set) => ({
+    email: '',
+    setEmail: (email: string) => set({ email }),
+
+    isEmailValid: true,
+    setIsEmailValid: (isEmailValid: boolean) => set({ isEmailValid }),
+
+    emailError: '',
+    setEmailError: (emailError: string) => set({ emailError }),
+
+    password: '',
+    setPassword: (password: string) => set({ password }),
+
+    isPasswordValid: true,
+    setIsPasswordValid: (isPasswordValid: boolean) => set({ isPasswordValid }),
+
+    passwordError: '',
+    setPasswordError: (passwordError: string) => set({ passwordError }),
+}));
+
+export default useLoginStore;
