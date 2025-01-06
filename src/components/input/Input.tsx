6ecfdc77efe5897ee43ea61463ce44b84389477a
kelvin <seungu.lee@g.austincc.edu>
2025-01-06 15:27:16 +0900

    Design: 비밀번호 input 태그 내부 비밀번호 표출 유무 아이콘 적용

--- src/components/input/Input.tsx
+++ src/components/input/Input.tsx
@@ -1,8 +1,10 @@
 import React from "react";
+import Button from "../button/Button";
 
 interface InputProps {
     type: string;
     value: string;
+    onClick?: () => void;
     onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
     onBlur: () => void;
     isValid: boolean;
@@ -13,6 +15,7 @@ interface InputProps {
 const Input: React.FC<InputProps> = ({
     type,
     value,
+    onClick,
     onChange,
     onBlur,
     isValid,
@@ -29,7 +32,10 @@ const Input: React.FC<InputProps> = ({
                 placeholder={placeholder}
                 className={!isValid ? 'error' : ''}
             />
-            <button type="reset" onClick={() => onChange({target: {value: ''}} as React.ChangeEvent<HTMLInputElement>)}></button>
+            {placeholder === "비밀번호를 입력해 주세요." && (
+              <Button type="button" className={`password ${type === 'text' ? 'active' : ''}`} onClick={onClick} />
+            )}
+            <Button type="reset" className={placeholder === "비밀번호를 입력해 주세요." ? 'reset-password' : 'reset'} onClick={() => onChange({target: {value: ''}} as React.ChangeEvent<HTMLInputElement>)} />
 
             {!isValid && errorMessage && (
                 <span className="error-message">{errorMessage}</span>
