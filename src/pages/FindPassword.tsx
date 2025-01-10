230094c0f1edd32a9b5cb190c4dfdaf08caf9cdb
kelvin <seungu.lee@g.austincc.edu>
2025-01-10 15:06:14 +0900

    Feat: 비밀번호 찾기 페이지 1차 퍼블 적용

---
+++ src/pages/FindPassword.tsx
@@ -0,0 +1,51 @@
+import React from "react";
+
+import Header from "../components/login/Header";
+import Input from "../components/input/Input";
+import Button from "../components/button/Button";
+
+import useLoginForm from "../hooks/login/useLoginForm";
+
+import "../styles/password/FindPassword.scss";
+
+const FindPassword = () => {
+    const {
+      email,
+      isEmailValid,
+      emailError,
+      handleEmailBlur,
+      handleEmailChange,
+    } = useLoginForm();
+
+    return (
+        <div className="find-password-container">
+            <div className="find-password-title">
+                <Header headerTitle="비밀번호 찾기"/>
+                <span>기존에 가입하신 이메일을 입력하시면, 비밀번호 변경 메일을 발송해 드립니다.</span>
+            </div>
+
+            <main>
+                <div className="input-container">
+                    <label>
+                        <Input
+                            type="text"
+                            value={email}
+                            onChange={handleEmailChange}
+                            onBlur={handleEmailBlur}
+                            isValid={isEmailValid}
+                            errorMessage={emailError}
+                            placeholder="이메일을 입력해 주세요."
+                        />
+                    </label>
+                    <Button
+                        className="get-email-button"
+                        title="비밀번호 변경 이메일 받기"
+                        onClick={() => console.log("비밀번호 변경 이메일 받기 !!")}
+                    />
+                </div>
+            </main>
+        </div>
+    );
+};
+
+export default FindPassword;
