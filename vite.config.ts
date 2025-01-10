33346063527d6b6fdcc653209b6323712f34e4ec
kelvin <seungu.lee@g.austincc.edu>
2025-01-10 12:49:11 +0900

    Chore: vite.config.ts 개발 서버 프록시 연결 설정 기본 틀 수정

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,23 @@
+import { fileURLToPath, URL } from 'node:url'
+
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-});
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
+  server: {
+    host: '0.0.0.0',
+    proxy: {
+      '/api': {
+        target: 'http://localhost:5173', /* TODO: 개발 서버 url로 변경 후 접속 연결 확인 필요 */
+        changeOrigin: true
+      }
+    }
+  }
+})
