9ed3967a78c6cd5b0ee7b2cb8c688e1622febf3d
kelvin <seungu.lee@g.austincc.edu>
2025-01-14 15:11:34 +0900

    Comment: 비밀번호 찾기 페이지 남은 작업 todo 관련 주석 추가

--- src/pages/password/FindPassword.tsx
+++ src/pages/password/FindPassword.tsx
@@ -36,10 +36,20 @@ const FindPassword = () => {
                             errorMessage={emailError}
                             placeholder="이메일을 입력해 주세요."
                         />
+                        {/*
+                          TODO: input에 입력된 이메일 정보가 db 정보랑 일치 하지 않은경우
+                                => 유효성 검증 로직 추가 필요 (useEmailForm.ts, useEmailStore.tsx 참고)
+                                => '일치하는 이메일 정보가 없습니다. 다시 확인해 주세요.' 문구 포함한 에러 메시지 표출 (피그마 디자인 참고 퍼블 필요)
+                         */}
                     </label>
                 </div>
 
                 <Button className="get-email-button" title="비밀번호 변경 이메일 받기" onClick={() => console.log("비밀번호 변경 이메일 받기 !!")}/>
+                {/*
+                  TODO: 비밀번호 변경 이메일 받기 버튼 클릭시
+                        => input에 입력된 이메일로 메일 전송 (백엔드 spring 메일 전송 api 추후 연동 필요)
+                        => '비밀번호 변경 메일이 발송되었습니다.' 문구 포함한 토스트 띄우기 (피그마 디자인 참고 퍼블 필요)
+                */}
             </main>
         </div>
     );
