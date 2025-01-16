bb02c20b90843c3369dca2eacb4194996cdad4e1
kelvin <seungu.lee@g.austincc.edu>
2025-01-16 17:00:13 +0900

    Design: 비밀번호 변경 완료 페이지 퍼블 추가

---
+++ src/pages/password/SuccessPassword.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { useNavigate } from "react-router";
+
+import Header from "../../components/login/Header";
+import Button from "../../components/button/Button";
+
+import "../../styles/password/SuccessPassword.scss";
+
+const SuccessPassword = () => {
+    const navigate = useNavigate();
+
+    return (
+        <div className="success-password-container">
+            <div className="success-password-title">
+                <Header className="Header" headerTitle="비밀번호 변경 완료" />
+                <span>비밀번호 변경이 완료되었습니다. 새로운 비밀번호로 로그인해 주세요.</span>
+            </div>
+
+            <Button className="move-login-button" title="로그인 화면으로" onClick={() => navigate('/')} />
+        </div>
+    );
+};
+
+export default SuccessPassword;
