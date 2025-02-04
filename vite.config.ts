cfbd6c0ae6a774ebaf311d4c360fbf3cc08fa3d4
kelvin <seungu.lee@g.austincc.edu>
2025-02-04 14:47:22 +0900

    Chore: vite.config.ts 개발 서버 프록시 연결 url 설정 변경 완료

--- vite.config.ts
+++ vite.config.ts
@@ -12,11 +12,10 @@ export default defineConfig({
     }
   },
   server: {
-    host: '0.0.0.0',
     proxy: {
       '/api': {
-        target: 'http://localhost:5173', /* TODO: 개발 서버 url로 변경 후 접속 연결 확인 필요 */
-        changeOrigin: true
+        target: 'http://220.93.95.108:13960',
+        changeOrigin: true,
       }
     }
   }
