216fdb40b3725d1538c1a3ed427017ce093323aa
taerin <kimtaerin0606@phidigital.co.kr>
2025-02-09 16:44:55 +0900

    Refactor: 회원가입 완료 페이지 퍼블리싱, 디자인 추가

--- src/pages/signup/SignUp.tsx
+++ src/pages/signup/SignUp.tsx
@@ -1,12 +1,18 @@
 import React from "react";
 import { useNavigate } from "react-router";
+import Header from "../../components/login/Header";
+import "../../styles/signup/SignUp.scss";
+import Button from "../../components/button/Button";
 
 const SignUp = () => {
     const navigate = useNavigate();
 
     return (
-        <div>
-            <div>Sign Up Page</div>
+        <div className="signup-container">
+            <div className="signup-title">
+                <Header className="Header" headerTitle="회원 가입 완료"/>
+                <span>[닉네임] 님의 회원가입이 성공적으로 완료되었습니다.</span>
+            </div>
             {/*
                 TODO: 회원 가입 완료 버튼 클릭시 localstorage 안에 저장되어 있던
                       1. 유저 정보 데이터 및 프로필 설정 완료된 데이터 백엔드로 api 태워서 보내고
@@ -15,7 +21,11 @@ const SignUp = () => {
                       1. 회원 가입 완료 버튼 클릭시 localstorage 안에 저장되어 있던 데이터 날림
                       2. 메인 페이지로 이동
             */}
-            <button onClick={() => {navigate('/'); localStorage.clear();}}>회원 가입 완료</button>
+            <Button
+                className="next"
+                title="로그인 화면으로"
+                onClick={() => navigate('/')}
+            />
         </div>
     );
 };
