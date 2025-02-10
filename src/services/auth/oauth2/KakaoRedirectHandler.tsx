3b7bbc557a1c0b37cfdfc5e4a5fc8bd18d161614
kelvin <seungu.lee@g.austincc.edu>
2025-02-10 14:03:52 +0900

    Refactor: OAuth2RedirectHandler.tsx 컴포넌트 명 변경 및 api 부분 수정

--- src/services/auth/oauth2/KakaoRedirectHandler.tsx
+++ src/services/auth/oauth2/KakaoRedirectHandler.tsx
@@ -1,25 +1,22 @@
 import React from "react";
 import { useNavigate } from "react-router";
 
-const OAuth2RedirectHandler = () => {
+const KakaoRedirectHandler = () => {
     const navigate = useNavigate();
 
     /* TODO: 백엔드에서 code 값을 다르게 내려주면 해당 값에 따라 KakaoLoginCallback.ts 내부에서 분기처리 진행 필요 */
-    /*
-        code: 1000 => 카카오로 로그인시 자체 서비스 신규 회원일 경우
-    */
     const code = new URLSearchParams(window.location.search).get('code');
 
     try {
         fetch(`/api/kakao/login?code=${code}`, {
-            method: 'GET',
+            method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
             }
         })
         .then(response => response.json())
         .then((response) => {
-            if (response.code === 1000) {
+            if (response.code === 2003) {
                 localStorage.clear();
                 localStorage.setItem('kakao_account', JSON.stringify(response.data.kakao_account));
                 navigate('/signup');
@@ -38,4 +35,4 @@ const OAuth2RedirectHandler = () => {
     );
 }
 
-export default OAuth2RedirectHandler;
+export default KakaoRedirectHandler;
