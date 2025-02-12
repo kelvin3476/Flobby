17b8f9125e3f31836979726aa56c1ce1f7f5ad53
kelvin <seungu.lee@g.austincc.edu>
2025-02-12 18:28:46 +0900

    Refactor: 라우터 > 회원가입 페이지 틀 => 회원가입 완료 페이지로 변경 및 지역 선택 페이지 위치 수정

--- src/pages/signup/SuccessSignUp.tsx
+++ src/pages/signup/SuccessSignUp.tsx
@@ -8,25 +8,28 @@ const SuccessSignUp = () => {
     const navigate = useNavigate();
 
     return (
-        <div className="signup-container">
-            <div className="signup-title">
-                <Header className="Header" headerTitle="회원 가입 완료"/>
-                <span>[닉네임] 님의 회원가입이 성공적으로 완료되었습니다.</span>
-            </div>
-            {/*
+      <div className="signup-container">
+        <div className="signup-title">
+          <Header className="Header" headerTitle="회원 가입 완료" />
+          <span>[닉네임] 님의 회원가입이 성공적으로 완료되었습니다.</span>
+        </div>
+        {/*
                 TODO: 회원 가입 완료 버튼 클릭시 localstorage 안에 저장되어 있던
                       1. 유저 정보 데이터 및 프로필 설정 완료된 데이터 백엔드로 api 태워서 보내고
-                      2. 성공시 메인 페이지로 이동
+                      2. 성공시 로그인 페이지로 이동
                       현재 임시 조치:
                       1. 회원 가입 완료 버튼 클릭시 localstorage 안에 저장되어 있던 데이터 날림
-                      2. 메인 페이지로 이동
+                      2. 로그인 페이지로 이동
             */}
-            <Button
-                className="next"
-                title="로그인 화면으로"
-                onClick={() => navigate('/')}
-            />
-        </div>
+        <Button
+          className="next"
+          title="로그인 화면으로"
+          onClick={() => {
+            navigate('/');
+            localStorage.clear();
+          }}
+        />
+      </div>
     );
 };
 
