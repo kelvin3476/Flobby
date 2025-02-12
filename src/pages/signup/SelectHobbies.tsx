d8149f9a0e7dc531949016ff5a8c2f922d6cf507
taerin <kimtaerin0606@phidigital.co.kr>
2025-02-12 13:10:57 +0900

    Refactor: 퍼블리싱 수정사항 반영 완료

--- src/pages/signup/SelectHobbies.tsx
+++ src/pages/signup/SelectHobbies.tsx
@@ -6,58 +6,56 @@ import Header from '../../components/login/Header';
 import '../../styles/signup/SelectHobbies.scss';
 import Button from '../../components/button/Button';
 
+
 const SelectHobbies = () => {
+  const { selectedHobbies, addHobby, removeHobby, hobbyCount,hideHobbyList,setHideHobbyList } = useHobbyStore();
   const navigate = useNavigate();
-  const { selectedHobbies, addHobby, removeHobby, hobbyCount } =
-    useHobbyStore();
-
-  const sports = [
-    '자전거',
-    '배드민턴',
-    '볼링',
-    '테니스',
-    '골프',
-    '클라이밍',
-    '탁구',
-    '러닝',
-    '축구',
-    '농구',
-    '야구',
-  ];
-  const languages = [
-    '영어',
-    '일본어',
-    '중국어',
-    '프랑스어',
-    '러시아어',
-    '독일어',
-    '튀르키에어',
-  ];
-  const network = ['지역', '나이', '파티', '맛집'];
-
-  const categoryData = [
-    { category: '스포츠', items: sports },
-    { category: '외국/언어', items: languages },
-    { category: '사교/인맥', items: network },
-    { category: '사교/인맥', items: network },
+  const CATEGORY_DATA = [
+    {
+      category: '스포츠',
+      items: ['자전거', '배드민턴', '볼링', '테니스', '골프', '클라이밍', '탁구', '러닝', '축구', '농구', '야구'],
+    },
+    {
+      category: '외국/언어',
+      items: ['영어', '일본어', '중국어', '프랑스어', '러시아어', '독일어', '튀르키에어'],
+    },
+    {
+      category: '사교/인맥',
+      items: ['지역', '나이', '파티', '맛집'],
+    },
   ];
 
-  const handleOnChange = (el: string) => {
-    addHobby(el);
-  };
+  const toggleHobbyListVisibility = (index:number) =>{
+    setHideHobbyList(index);
+  }
 
-  const deleteHobby = (el: string) => {
-    removeHobby(el);
-  };
+  const selectHobbyToggle = (hobby : string) =>{
+    selectedHobbies.includes(hobby) ? removeHobby(hobby) : addHobby(hobby);
+  }
+
+  const isHobbyListHidden = (index: number) => hideHobbyList.includes(index);
+
+  const isNextButtonDisabled = hobbyCount === 0;
+
+  //선택된 취미를 렌더링
+  const renderSelectedHobbies = ()=> {
+    return (<div className="selected-box">
+          <ul>
+            {selectedHobbies.map((el, idx) => (
+                <li key={idx}>
+                  <span className="text"> {el} </span>
+                  <span className="delete" onClick={() => removeHobby(el)}></span>
+                </li>
+            ))}
+          </ul>
+        </div>
+    )};
 
   return (
     <div className="hobby-container">
       <div className="hobby-title">
-        <Header
-          className="Header"
-          headerTitle="어떤 취미에 관심이 있으신가요?"
-        />
-        <span className={hobbyCount == 3 ? 'max' : ''}>
+        <Header className="Header" headerTitle="어떤 취미에 관심이 있으신가요?"/>
+        <span className={hobbyCount === 3  ? 'max' : ''}>
           관심 취미는 최대 3개까지 선택할 수 있어요. ({hobbyCount}/3)
         </span>
       </div>
@@ -66,49 +64,45 @@ const SelectHobbies = () => {
         {/*취미 카테고리, 리스트*/}
         <div className="hobby">
           <div className="category">
-            {categoryData.map((categoryObj, index) => (
-              <div key={index}>
-                <div className="title">
-                  <span className="circle"></span>
-                  <span className="text">{categoryObj.category}</span>
-                  <button
-                    className="hide"
-                    onClick={() => console.log(index)} //todo
-                  ></button>
+            {CATEGORY_DATA.map((categoryObj, index) => (
+                <div key={index}>
+                  <div className="title">
+                    <span className="circle"></span>
+                    <span className="text">{categoryObj.category}</span>
+                    <button
+                        className={isHobbyListHidden(index)? 'hide' : 'show'}
+                        onClick={() => toggleHobbyListVisibility(index)}
+                    ></button>
+                  </div>
+                  <ul className={`hobby-ul ${isHobbyListHidden(index)? 'hide' : ''}`}>
+                      {categoryObj.items.map((item, idx) => (
+                          <li key={idx}>
+                            <label
+                                htmlFor={item}
+                                className={selectedHobbies.includes(item) ? 'checked' : ''}>
+                              {item}
+                            </label>
+                            <input
+                                id={item}
+                                type="checkbox"
+                                checked={selectedHobbies.includes(item)}
+                                onChange={() => selectHobbyToggle(item)}
+                            />
+                          </li>
+                      ))}
+                    </ul>
                 </div>
-                <ul className="hobby-ul">
-                  {categoryObj.items.map((el, idx) => (
-                    <li key={idx}>
-                      <label
-                        htmlFor={el}
-                        className={
-                          selectedHobbies.includes(el) ? 'checked' : ''
-                        }
-                      >
-                        {el}
-                      </label>
-                      <input
-                        id={el}
-                        type="checkbox"
-                        checked={selectedHobbies.includes(el)}
-                        onChange={() => handleOnChange(el)}
-                      />
-                    </li>
-                  ))}
-                </ul>
-              </div>
-            ))}
+              ))}
           </div>
 
           {/*선택된 취미*/}
+          {renderSelectedHobbies()}
           <div className="selected-box">
             <ul>
-              {selectedHobbies.map((el, idx) => (
+              {selectedHobbies.map((hobby, idx) => (
                 <li key={idx}>
-                  <span className="text"> {el} </span>
-                  <span
-                    className="delete"
-                    onClick={() => deleteHobby(el)}
+                  <span className="text"> {hobby} </span>
+                  <span className="delete" onClick={() => removeHobby(hobby)}
                   ></span>
                 </li>
               ))}
@@ -117,9 +111,9 @@ const SelectHobbies = () => {
         </div>
         <div className="buttons">
           <Button
-            className="next"
-            title="다음"
-            onClick={() => navigate('/next')}
+              className={isNextButtonDisabled ? 'next disable' : 'next'}
+              title="다음"
+              onClick={() => !isNextButtonDisabled && navigate('/next')}
           />
           <Button
             className="skip"
@@ -131,5 +125,6 @@ const SelectHobbies = () => {
     </div>
   );
 };
+
 
 export default SelectHobbies;
--- src/store/signup/useHobbyStore.tsx
+++ src/store/signup/useHobbyStore.tsx
@@ -3,13 +3,16 @@ import { create } from 'zustand';
 interface HobbyStore {
   selectedHobbies: string[];
   hobbyCount: number;
+  hideHobbyList: number[];
   addHobby: (hobby: string) => void;
   removeHobby: (hobby: string) => void;
+  setHideHobbyList: (index: number) => void;
 }
 
 const useHobbyStore = create<HobbyStore>(set => ({
   selectedHobbies: [],
   hobbyCount: 0,
+  hideHobbyList:[],
   addHobby: hobby => set(state => {
       // hobbyCount가 3 이상이면 추가하지 않음
       if (state.hobbyCount >= 3 || state.selectedHobbies.includes(hobby)) {
@@ -20,8 +23,7 @@ const useHobbyStore = create<HobbyStore>(set => ({
           hobbyCount: state.hobbyCount + 1, // hobbyCount 증가
       };
     }),
-  removeHobby: hobby =>
-    set(state => {
+  removeHobby: hobby => set(state => {
       const updatedHobbies = state.selectedHobbies.filter(
         item => item !== hobby,
       );
@@ -29,6 +31,14 @@ const useHobbyStore = create<HobbyStore>(set => ({
         selectedHobbies: updatedHobbies,
         hobbyCount: state.hobbyCount - 1,
       };
+    }),
+    setHideHobbyList : index => set(state=> {
+        if(state.hideHobbyList.includes(index)){
+            return {hideHobbyList : state.hideHobbyList.filter((prev) => prev !=index)}
+        }
+        return {
+            hideHobbyList: [...state.hideHobbyList, index]
+        };
     }),
 }));
 
