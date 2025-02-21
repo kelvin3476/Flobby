0d45a919d13142728d47aa93327961dd6369b0b5
ongzzzz <kyjj0506@naver.com>
2025-02-22 00:58:28 +0900

    Feat: 로딩 스피너 리다이렉트 페이지 우선적용

--- src/services/auth/oauth2/KakaoRedirectHandler.tsx
+++ src/services/auth/oauth2/KakaoRedirectHandler.tsx
@@ -2,8 +2,11 @@ import React from "react";
 import { useNavigate } from "react-router";
 import SocialLogin from "../../../api/login/SocialLogin";
 // import Login from '../../../api/login/Login';
+import useLoadingForm from "../../../hooks/loading/useLoadingForm";
 
 const KakaoRedirectHandler = () => {
+    const {isLoading, loadingSVG} = useLoadingForm();
+
     const navigate = useNavigate();
 
     /* TODO: 백엔드에서 code 값을 다르게 내려주면 해당 값에 따라 KakaoLoginCallback.ts 내부에서 분기처리 진행 필요 */
@@ -60,7 +63,11 @@ const KakaoRedirectHandler = () => {
     }
 
     return (
-        <div>로그인 중...</div>
+        <>
+            <div className={`loading-container ${isLoading ? "active" : ""}`}>
+                <img src={loadingSVG} alt="로딩 중..." />
+            </div>
+        </>
     );
 }
 
