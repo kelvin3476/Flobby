1042c87354cc4e69d739f4ec2881d52f696c7065
kelvin <seungu.lee@g.austincc.edu>
2025-03-05 18:11:38 +0900

    Refactor: 회원가입 성공 페이지 > 회원가입 성공 페이지 기능 커스텀 훅 적용

--- src/pages/signup/SuccessSignUp.tsx
+++ src/pages/signup/SuccessSignUp.tsx
@@ -1,33 +1,25 @@
 import React from "react";
-import { useNavigate } from "react-router";
+
 import Header from "../../components/login/Header";
-import "../../styles/signup/SuccessSignUp.scss";
 import Button from "../../components/button/Button";
 
+import "../../styles/signup/SuccessSignUp.scss";
+
+import useSuccessSignupForm from "../../hooks/signup/success/useSuccessSignupForm";
+
 const SuccessSignUp = () => {
-    const navigate = useNavigate();
+    const { finalSocialSignUpHandler, finalSignUpHandler } = useSuccessSignupForm();
 
     return (
       <div className="signup-container">
         <div className="signup-title">
           <Header className="Header" headerTitle="회원 가입 완료" />
-          <span>[닉네임] 님의 회원가입이 성공적으로 완료되었습니다.</span>
+          <span>[{localStorage.getItem('nickname')}] 님의 회원가입이 성공적으로 완료되었습니다.</span>
         </div>
-        {/*
-                TODO: 회원 가입 완료 버튼 클릭시 localstorage 안에 저장되어 있던
-                      1. 유저 정보 데이터 및 프로필 설정 완료된 데이터 백엔드로 api 태워서 보내고
-                      2. 성공시 로그인 페이지로 이동
-                      현재 임시 조치:
-                      1. 회원 가입 완료 버튼 클릭시 localstorage 안에 저장되어 있던 데이터 날림
-                      2. 로그인 페이지로 이동
-            */}
         <Button
           className="next"
           title="로그인 화면으로"
-          onClick={() => {
-            navigate('/');
-            localStorage.clear();
-          }}
+          onClick={localStorage.getItem('socialType') ? finalSocialSignUpHandler : finalSignUpHandler}
         />
       </div>
     );
