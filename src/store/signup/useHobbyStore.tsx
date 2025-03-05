c3e94b1df8ad143392839b8c5d6be1400a5b4fce
kelvin <seungu.lee@g.austincc.edu>
2025-03-05 17:06:58 +0900

    Refactor: 회원가입 > 취미 선택 상태관리 스토어 > 취미 선택시 로컬스토리지에 저장 하게끔 로직 수정

--- src/store/signup/useHobbyStore.tsx
+++ src/store/signup/useHobbyStore.tsx
@@ -1,4 +1,5 @@
 import { create } from 'zustand';
+import { persist, createJSONStorage } from 'zustand/middleware';
 
 interface HobbyStore {
   selectedHobbies: string[];
@@ -9,37 +10,50 @@ interface HobbyStore {
   setHideHobbyList: (index: number) => void;
 }
 
-const useHobbyStore = create<HobbyStore>(set => ({
-  selectedHobbies: [],
-  hobbyCount: 0,
-  hideHobbyList:[],
-  addHobby: hobby => set(state => {
-      // hobbyCount가 3 이상이면 추가하지 않음
-      if (state.hobbyCount >= 3 || state.selectedHobbies.includes(hobby)) {
-          return state;
-      }
-      return {
-          selectedHobbies: [...state.selectedHobbies, hobby],
-          hobbyCount: state.hobbyCount + 1, // hobbyCount 증가
-      };
+const useHobbyStore = create<HobbyStore>()(
+  persist(
+    (set, get) => ({
+      selectedHobbies: [],
+      hobbyCount: 0,
+      hideHobbyList: [],
+      addHobby: hobby =>
+        set(state => {
+          // hobbyCount가 3 이상이면 추가하지 않음
+          if (state.hobbyCount >= 3 || state.selectedHobbies.includes(hobby)) {
+            return state;
+          }
+          return {
+            selectedHobbies: [...state.selectedHobbies, hobby],
+            hobbyCount: state.hobbyCount + 1, // hobbyCount 증가
+          };
+        }),
+      removeHobby: hobby =>
+        set(state => {
+          const updatedHobbies = state.selectedHobbies.filter(
+            item => item !== hobby,
+          );
+          return {
+            selectedHobbies: updatedHobbies,
+            hobbyCount: state.hobbyCount - 1,
+          };
+        }),
+      setHideHobbyList: index =>
+        set(state => {
+          if (state.hideHobbyList.includes(index)) {
+            return {
+              hideHobbyList: state.hideHobbyList.filter(prev => prev != index),
+            };
+          }
+          return {
+            hideHobbyList: [...state.hideHobbyList, index],
+          };
+        }),
     }),
-  removeHobby: hobby => set(state => {
-      const updatedHobbies = state.selectedHobbies.filter(
-        item => item !== hobby,
-      );
-      return {
-        selectedHobbies: updatedHobbies,
-        hobbyCount: state.hobbyCount - 1,
-      };
-    }),
-    setHideHobbyList : index => set(state=> {
-        if(state.hideHobbyList.includes(index)){
-            return {hideHobbyList : state.hideHobbyList.filter((prev) => prev !=index)}
-        }
-        return {
-            hideHobbyList: [...state.hideHobbyList, index]
-        };
-    }),
-}));
+    {
+      name: 'hobby-storage',
+      storage: createJSONStorage(() => localStorage),
+    },
+  ),
+);
 
 export default useHobbyStore;
