8b17a74bfade53c9eb9a08f2b5c3a0c2b6a3b878
ongzzzz <kyjj0506@naver.com>
2025-03-14 18:08:54 +0800

    Refactor: 회원가입 지역 페이지 api 연동

--- src/hooks/signup/useRegionForm.ts
+++ src/hooks/signup/useRegionForm.ts
@@ -1,20 +1,24 @@
-import React from "react";
-import useRegionStore from "../../store/signup/useRegionStore";
-
+import React, { useEffect, useRef } from "react";
 import { useNavigate } from "react-router";
+
+import useRegionStore from "../../store/signup/useRegionStore";
+import SignUp from "../../api/signup/SignUp";
 
 const useRegionForm = () => {
   const { 
     selectedRegions, 
     activeCity, 
-    setActiveCity, 
+    setActiveCity: originalSetActiveCity, 
     attemptSelectRegion, 
     removeRegion, 
     warning,
     getRegions,
+    cityDistrictMap,
+    setCityDistrictMap,
   } = useRegionStore();
 
   const nav = useNavigate();
+  const townRef = useRef<HTMLDivElement>(null);
 
   const handleSelect = (city: string, district: string) => {
     const region = `${city} ${district}`;
@@ -27,6 +31,50 @@ const useRegionForm = () => {
     }
   };
 
+  const setActiveCity = (city: string) => {
+    originalSetActiveCity(city);
+    if (townRef.current) {
+      townRef.current.scrollTop = 0;
+    }
+  };
+
+  useEffect(() => {
+    const fetchRegions = async() => {
+
+      try {
+        const response = await SignUp.getRegionList();
+
+        const { code, message, data } = response.data;
+
+        if (code === 1000) {
+          // API 호출 성공
+          const formattedData: Record<string, string[]> = {};
+
+          data.forEach(({ regionName, parentName }) => {
+            if (parentName) {
+              if (!formattedData[parentName]) {
+                formattedData[parentName] = [];
+              }
+              formattedData[parentName].push(regionName);
+            }
+          });
+
+          setCityDistrictMap(formattedData);
+        } else if (code === 1001) {
+          // API 호출 실패
+          throw new Error(message || "데이터를 가져오지 못했습니다.");
+        } else if (code === 1002) {
+          // API 예외 발생
+          throw new Error(message || "서버 오류가 발생했습니다.");
+        }
+      } catch (err: any) {
+        console.log(err.message || "데이터 로드 실패");
+      }
+    };
+
+    fetchRegions();
+  }, [setCityDistrictMap]);
+
   return {
     selectedRegions, 
     activeCity, 
@@ -37,6 +85,8 @@ const useRegionForm = () => {
     getRegions,
     handleSelect,
     acceptNext,
+    cityDistrictMap,
+    townRef,
   };
 };
 
