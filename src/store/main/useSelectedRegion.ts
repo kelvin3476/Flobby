5471ec3d9740edc2451c3610dd64b0f21cca29c5
bluemin <zeem_m2@naver.com>
2025-04-04 17:55:02 +0900

    Feat: 선택된 지역 전역 상태 관리

---
+++ src/store/main/useSelectedRegion.ts
@@ -0,0 +1,16 @@
+import { create } from 'zustand';
+
+interface SelectedRegionStore {
+  selectedRegion: string | null;
+  setSelectedRegion: (region: string) => void;
+}
+
+const selectedRegions = ['송파구', '구로구', '용산구'];
+
+const useSelectedRegion = create<SelectedRegionStore>()(set => ({
+  // TODO: 로그인 : 초기값 유저 정보 api 호출 필요 / 비로그인 : 서울 전체(백에서 api 수정 예정)
+  selectedRegion: selectedRegions[0],
+  setSelectedRegion: region => set({ selectedRegion: region }),
+}));
+
+export default useSelectedRegion;
