7fbac7035cf545ae3fae58b370a5008f7fb38185
bluemin <zeem_m2@naver.com>
2025-04-04 18:20:28 +0900

    Comment: 테스트용 주석 추가

--- src/store/main/useSelectedRegion.ts
+++ src/store/main/useSelectedRegion.ts
@@ -5,7 +5,7 @@ interface SelectedRegionStore {
   setSelectedRegion: (region: string) => void;
 }
 
-const selectedRegions = ['송파구', '구로구', '용산구'];
+const selectedRegions = ['송파구', '구로구', '용산구']; // test용 초기값
 
 const useSelectedRegion = create<SelectedRegionStore>()(set => ({
   // TODO: 로그인 : 초기값 유저 정보 api 호출 필요 / 비로그인 : 서울 전체(백에서 api 수정 예정)
