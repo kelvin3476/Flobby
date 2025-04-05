e4bd82e2f8d59a852fce0f610941d26f11905636
bluemin <zeem_m2@naver.com>
2025-04-05 20:25:10 +0900

    Comment: 주석 제거

--- src/hooks/search_bar/useSearchBarHandlers.ts
+++ src/hooks/search_bar/useSearchBarHandlers.ts
@@ -6,7 +6,6 @@ export default function useSearchBarHandlers() {
 
   const [isTyping, setIsTyping] = useState(false);
 
-  // search-form submit event handler
   const handleSubmitSearchForm = (e: React.FormEvent<HTMLElement>) => {
     e.preventDefault();
     if (searchKeyword.length > 0) {
@@ -14,30 +13,25 @@ export default function useSearchBarHandlers() {
 
       // TODO: 검색 api 연결
 
-      setIsTyping(false); // reset-button disable
+      setIsTyping(false);
     }
   };
 
-  // search-input event handler
-  // onChange
   const handleChangeSearchInput = (e: React.ChangeEvent<HTMLInputElement>) => {
     setSearchKeyword(e.target.value);
     e.target.value.length > 0 ? setIsTyping(true) : setIsTyping(false);
   };
 
-  // onFocus
   const handleFocusSearchInput = () => {
     if (searchKeyword.length > 0) setIsTyping(true);
   };
 
-  // onBlur
   const handleBlurSearchInput = () => {
     setTimeout(() => {
       setIsTyping(false);
     }, 100);
   };
 
-  // reset-button event handler
   const handleResetSearchKeyword = () => {
     setSearchKeyword('');
     setIsTyping(false);
