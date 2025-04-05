d0b082609a9ba0562df25e9d62daa980c75b5161
bluemin <zeem_m2@naver.com>
2025-04-05 19:50:27 +0900

    Refactor: SearchBarEventHandler 함수 커스텀 훅으로 분리

--- src/components/main/SearchBar.tsx
+++ src/components/main/SearchBar.tsx
@@ -2,43 +2,18 @@ import React, { useState } from 'react';
 import useSearchKeyword from '../../store/main/useSearchKeyword';
 
 import '../../styles/main/search_bar/SearchBar.scss';
+import useSearchBarHandlers from '../../hooks/search_bar/useSearchBarHandlers';
 
 const SearchBar: React.FC = () => {
-  const { searchKeyword, setSearchKeyword } = useSearchKeyword();
-
-  const [isTyping, setIsTyping] = useState<boolean>(false);
-
-  const handleSubmitSearchForm = (e: React.FormEvent<HTMLElement>) => {
-    e.preventDefault();
-
-    if (searchKeyword.length > 0) {
-      console.log(searchKeyword, '검색어 제출!'); // test용
-      // TODO: 검색 api 연결
-
-      setIsTyping(false);
-    }
-  };
-
-  const handleChangeSearchInput = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setSearchKeyword(e.target.value);
-
-    e.target.value.length > 0 ? setIsTyping(true) : setIsTyping(false);
-  };
-
-  const handleResetSearchKeyword = () => {
-    setSearchKeyword('');
-    setIsTyping(false);
-  };
-
-  const handleFocusSearchInput = () => {
-    searchKeyword.length > 0 && setIsTyping(true);
-  };
-
-  const handleBlurSearchInput = () => {
-    setTimeout(() => {
-      setIsTyping(false);
-    }, 100);
-  };
+  const {
+    searchKeyword,
+    isTyping,
+    handleSubmitSearchForm,
+    handleChangeSearchInput,
+    handleResetSearchKeyword,
+    handleFocusSearchInput,
+    handleBlurSearchInput,
+  } = useSearchBarHandlers();
 
   return (
     <form className="search-bar-form" onSubmit={handleSubmitSearchForm}>
---
+++ src/hooks/search_bar/useSearchBarHandlers.ts
@@ -0,0 +1,55 @@
+import { useState } from 'react';
+import useSearchKeyword from '../../store/main/useSearchKeyword';
+
+export default function useSearchBarHandlers() {
+  const { searchKeyword, setSearchKeyword } = useSearchKeyword();
+
+  const [isTyping, setIsTyping] = useState(false);
+
+  //   search-form submit event handler
+  const handleSubmitSearchForm = (e: React.FormEvent<HTMLElement>) => {
+    e.preventDefault();
+    if (searchKeyword.length > 0) {
+      console.log(searchKeyword, '검색어 제출!'); // test
+
+      // TODO: 검색 api 연결
+
+      setIsTyping(false); // reset-button disable
+    }
+  };
+
+  //   search-input event handler
+  //   onChange
+  const handleChangeSearchInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchKeyword(e.target.value);
+    e.target.value.length > 0 ? setIsTyping(true) : setIsTyping(false);
+  };
+
+  //   onFocus
+  const handleFocusSearchInput = () => {
+    if (searchKeyword.length > 0) setIsTyping(true);
+  };
+
+  //   onBlur
+  const handleBlurSearchInput = () => {
+    setTimeout(() => {
+      setIsTyping(false);
+    }, 100);
+  };
+
+  //   reset-button event handler
+  const handleResetSearchKeyword = () => {
+    setSearchKeyword('');
+    setIsTyping(false);
+  };
+
+  return {
+    searchKeyword,
+    isTyping,
+    handleSubmitSearchForm,
+    handleChangeSearchInput,
+    handleResetSearchKeyword,
+    handleFocusSearchInput,
+    handleBlurSearchInput,
+  };
+}
