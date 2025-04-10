14b3f6d1dfda7eea5f505c49df8938b7a40111f6
kelvin <seungu.lee@g.austincc.edu>
2025-04-10 19:58:29 +0900

    Feat: 메인 페이지 컴포넌트 틀 추가 (Main.tsx)

---
+++ src/pages/main/Main.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Main = () => {
+    return (
+        <main>
+            {/*
+                TODO: 메인 페이지 컴포넌트에 각자 작업한 컴포넌트 병합 작업 필요
+                      메인 페이지 헤더 부분 작업 및 병합 후 포지셔닝 작업 필요 (연지)
+                      포지셔닝 작업 후 반응형 작업 필요 (승우)
+             */}
+        </main>
+    );
+};
+
+export default Main;
