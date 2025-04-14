d854dbb7fe02dea1e83dcdc9a211de1eb2aa53e6
bluemin <zeem_m2@naver.com>
2025-04-14 11:53:24 +0900

    Test: 테스트용 목데이터 추가 및 초기값 설정 로직 추가

--- src/components/main/region_selector/RegionSelector.tsx
+++ src/components/main/region_selector/RegionSelector.tsx
@@ -4,11 +4,17 @@ import useSelectedRegionStore from '../../../store/main/useSelectedRegionStore';
 
 import '../../../styles/main/region_selector/RegionSelector.scss';
 
+// 관심 지역 test용 초기값
+
 const preferRegions = [
   { regionName: '송파구', regionId: 195 },
   { regionName: '구로구', regionId: 203 },
   { regionName: '용산구', regionId: 216 },
-]; // 관심 지역 test용 초기값
+];
+
+// const preferRegions = [];
+
+// const preferRegions = null;
 
 const RegionSelector: React.FC = () => {
   const { selectedRegion, setSelectedRegion } = useSelectedRegionStore();
@@ -32,6 +38,12 @@ const RegionSelector: React.FC = () => {
       document.removeEventListener('mousedown', handleClickOutside);
     };
   }, []);
+
+  useEffect(() => {
+    if (!preferRegions || preferRegions?.length === 0) {
+      setSelectedRegion({ regionName: '서울전체', regionId: 288 });
+    }
+  }, [preferRegions]);
 
   return (
     <div className="region-selector">
