1999c9b756460bbffc28da0d39a3624bb00be1f1
kelvin <seungu.lee@g.austincc.edu>
2025-05-02 17:01:21 +0900

    Refactor: Main.tsx > useMainPage.ts 추가 (추후 accessToken을 prop으로 넘겨서 메인페이지 로그인 상태 확인 처리 및 분기 처리 예정)

--- src/pages/main/Main.tsx
+++ src/pages/main/Main.tsx
@@ -1,16 +1,27 @@
 import React from 'react';
 
+import logger from '../../utils/Logger';
+
 import CarouselBanner from '../../components/carousel/CarouselBanner';
 import ClubPost from '../../components/main/club/ClubPost';
 import PopularPost from '../../components/main/popular_post/PopularPost';
 import MainHeader from '../../components/header/MainHeader';
 
+import useMainPage from '../../hooks/main/useMainPage';
+
 const Main = () => {
+  const {
+    accessToken,
+  } = useMainPage();
+
+  logger.log('[accessToken]', accessToken);
+
   return (
     <main>
       {/*
                 TODO: 메인 페이지 컴포넌트에 각자 작업한 컴포넌트 병합 작업 필요
-                      메인 페이지 헤더 부분 작업 및 병합 후 포지셔닝 작업 필요 (연지)
+                      메인 페이지 헤더 부분 작업 (연지)
+                      병합 후 포지셔닝 작업 필요 (지민)
                       포지셔닝 작업 후 반응형 작업 필요 (승우)
              */}
       <MainHeader />
