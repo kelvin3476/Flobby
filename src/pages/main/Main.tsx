2584ef4ac46d07721c6f5d760e336cb67f21fdca
kelvin <seungu.lee@g.austincc.edu>
2025-05-08 14:32:28 +0900

    Design: 메인 페이지 반응형 디자인 적용시 필요한 부모 컨테이너 추가

--- src/pages/main/Main.tsx
+++ src/pages/main/Main.tsx
@@ -18,19 +18,15 @@ const Main = () => {
   logger.log('[accessToken]', accessToken);
 
   return (
-    <main>
-      {/*
-                TODO: 메인 페이지 컴포넌트에 각자 작업한 컴포넌트 병합 작업 필요
-                      메인 페이지 헤더 부분 작업 (연지)
-                      병합 후 포지셔닝 작업 필요 (지민)
-                      포지셔닝 작업 후 반응형 작업 필요 (승우)
-             */}
-      <MainHeader />
-      <CarouselBanner />
-      <ClubPost />
-      <OnedayPost />
-      <PopularPost />
-    </main>
+    <div className="responsive-container">
+      <main>
+        <MainHeader />
+        <CarouselBanner />
+        <ClubPost />
+        <OnedayPost />
+        <PopularPost />
+      </main>
+    </div>
   );
 };
 
