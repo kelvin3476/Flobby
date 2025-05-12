c2b529afb664a89aad216803f915237504747f68
ongzzzz <kyjj0506@naver.com>
2025-05-12 16:12:00 +0800

    Design: 모임 소개 컴포넌트 생성

---
+++ src/components/club/create/ClubDescription.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import Label from "./Label";
+
+import "../../../styles/club/create/ClubDescription.scss";
+
+const ClubDescription = () => {
+  return (
+    <div className="desc-container">
+      <div className="up-wrapper">
+        <Label labelTitle="모임 소개"/>
+        <input 
+          type="text"
+          placeholder="제목을 입력해 주세요."
+          className="club-title-input"
+          maxLength={14}
+        />
+      </div>
+      <textarea 
+        name="clubDescription" 
+        placeholder={`게시한 글에 욕설, 음란물 및 비방적인 내용의 글이나 사진, 영상이 포함될 경우,\n사전 예고없이 삭제 또는 비공개 처리될 수 있음을 알려 드립니다.`}
+        className="club-description-textarea"
+        maxLength={1000}
+      ></textarea>
+    </div>
+  );
+};
+
+export default ClubDescription;
