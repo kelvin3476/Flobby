5c3806c0240f8a21c331def1e365bdddf89bb563
ongzzzz <kyjj0506@naver.com>
2025-05-12 16:54:14 +0800

    Design: 자동 승인 여부 컴포넌트 생성

---
+++ src/components/club/create/ClubAuto.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import Label from "./Label";
+
+import "../../../styles/club/create/ClubAuto.scss";
+
+const ClubAuto = () => {
+  return (
+    <div className="auto-container">
+      <Label labelTitle="자동 승인 여부"/>
+      <div className="select-wrapper">
+        <label className="yes-label">
+          <input type="radio" name="autoApproval" value="true" />
+          Y
+        </label>
+        <label className="no-label">
+          <input type="radio" name="autoApproval" value="false" />
+          N
+        </label>
+      </div>
+    </div>
+  );
+};
+
+export default ClubAuto;
