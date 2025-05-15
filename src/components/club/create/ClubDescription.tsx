7ed7cfae4902ba92ba70474f77a0f67a260880d8
ongzzzz <kyjj0506@naver.com>
2025-05-15 19:11:31 +0800

    Feat: 유효성 검사 및 카운팅 기능 추가

--- src/components/club/create/ClubDescription.tsx
+++ src/components/club/create/ClubDescription.tsx
@@ -11,7 +11,48 @@ const ClubDescription = () => {
     setClubName,
     description,
     setDescription,
+    isTitleValid,
+    setIsTitleValid,
+    isDescValid,
+    setIsDescValid,
+    descCount,
+    setDescCount,
+    titleError,
+    setTitleError,
+    descError,
+    setDescError,
   } = useClubCreateStore();
+
+  const handleTitleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setClubName(e.target.value);
+    
+    if(e.target.value.trim() === "") {
+      setIsTitleValid(false);
+      setTitleError("모임 제목을 입력해 주세요.");
+    } else if (e.target.value.length >= 14) {
+      setIsTitleValid(false);
+      setTitleError("제목은 최대 14자까지 작성할 수 있어요.");
+    } else {
+      setIsTitleValid(true);
+      setTitleError("");
+    }
+  }
+
+  const handleDescChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setDescription(e.target.value);
+    setDescCount(e.target.value.length);
+    
+    if(e.target.value.trim() === "") {
+      setIsDescValid(false);
+      setDescError("소개글을 입력해 주세요.");
+    } else if (e.target.value.length >= 1000) {
+      setIsDescValid(false);
+      setDescError("소개글은 최대 1,000자까지 작성할 수 있어요.");
+    } else {
+      setIsDescValid(true);
+      setDescError("");
+    }
+  }
 
   return (
     <div className="desc-container">
@@ -23,17 +64,28 @@ const ClubDescription = () => {
           className="club-title-input"
           maxLength={14}
           value={clubName}
-          onChange={(e) => setClubName(e.target.value)}
+          onChange={handleTitleChange}
         />
+        {!isTitleValid && (
+          <span className="err-message">{titleError}</span>
+        )}
       </label>
-      <textarea 
-        name="clubDescription" 
-        placeholder={`게시한 글에 욕설, 음란물 및 비방적인 내용의 글이나 사진, 영상이 포함될 경우,\n사전 예고없이 삭제 또는 비공개 처리될 수 있음을 알려 드립니다.`}
-        className="club-description-textarea"
-        maxLength={1000}
-        value={description}
-        onChange={(e) => setDescription(e.target.value)}
-      ></textarea>
+      <div className="desc-wrapper">
+        <div className="area-wrapper">
+          <textarea 
+            name="clubDescription" 
+            placeholder={`게시한 글에 욕설, 음란물 및 비방적인 내용의 글이나 사진, 영상이 포함될 경우,\n사전 예고없이 삭제 또는 비공개 처리될 수 있음을 알려 드립니다.`}
+            className="club-description-textarea"
+            maxLength={1000}
+            value={description}
+            onChange={handleDescChange}
+          ></textarea>
+          <div className="text-count">{`(${descCount}/1,000자)`}</div>
+        </div>
+        {!isDescValid && (
+          <span className="err-message">{descError}</span>
+        )}
+      </div>
     </div>
   );
 };
