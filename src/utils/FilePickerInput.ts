4046d60f099197dbfecb1cb9d3d7df0b81d8d1b0
bluemin <zeem_m2@naver.com>
2025-05-16 01:03:55 +0900

    Remove: 불필요한 toast 관련 코드 삭제

--- src/utils/FilePickerInput.ts
+++ src/utils/FilePickerInput.ts
@@ -1,21 +1,15 @@
 import logger from './Logger';
 
-// toast 관련 로직은 주석처리
-
 export default class FilePickerInput<T> {
   private fileInput: HTMLInputElement;
   private fileUploader: (file: File[]) => Promise<T[]>;
-  //   private isShowToast = true;
 
-  // 생성자
   constructor(
     fileInput: HTMLInputElement,
     fileUploader: (file: File[]) => Promise<T[]>,
-    // isShowToast = true,
   ) {
     this.fileInput = fileInput;
     this.fileUploader = fileUploader;
-    // this.isShowToast = isShowToast;
   }
 
   // 파일 선택 및 업로드 실행
@@ -57,66 +51,20 @@ export default class FilePickerInput<T> {
     reject: any,
     fileFilter?: (files: File[]) => Promise<File[]>,
   ) {
-    // const time = new Date().getTime();
-    // this.sendToastMessage(
-    //   '파일을 업로드 중입니다.',
-    //   ToastPopupBgColor.GRAY,
-    //   time,
-    // );
-    // do filter
     const fileList = fileFilter ? await fileFilter(files) : files;
 
     if (fileList.length === 0) {
-      //   this.removeToastMessage(
-      //     '파일을 업로드 중입니다.',
-      //     ToastPopupBgColor.GRAY,
-      //     time,
-      //   );
       resolve([]);
       return;
     }
 
     this.fileUploader(fileList)
       .then(urls => {
-        // this.removeToastMessage(
-        //   '파일을 업로드 중입니다.',
-        //   ToastPopupBgColor.GRAY,
-        //   time,
-        // );
         resolve(urls);
       })
       .catch(e => {
         logger.error('cannot upload file', e);
-
         reject(e);
       });
   }
-
-  // EventBus 토스트 메시지 띄우기
-  //   private sendToastMessage(
-  //     message: string,
-  //     bgColor = ToastPopupBgColor.GRAY,
-  //     time: number,
-  //   ) {
-  //     if (!this.isShowToast) return;
-  //     EventBus.$emit('setToastPopUp', {
-  //       bgColor: bgColor,
-  //       title: message,
-  //       time: time,
-  //     });
-  //   }
-
-  // EventBus 토스트 메시지 지우기
-  //   private removeToastMessage(
-  //     message: string,
-  //     bgColor = ToastPopupBgColor.GRAY,
-  //     time: number,
-  //   ) {
-  //     if (!this.isShowToast) return;
-  //     EventBus.$emit('removeToastPopUp', {
-  //       bgColor: bgColor,
-  //       title: message,
-  //       time: time,
-  //     });
-  //   }
 }
