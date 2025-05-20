106b0450af6ff0f43b17a39fe5b72a98602ff67a
ongzzzz <kyjj0506@naver.com>
2025-05-21 01:38:57 +0800

    Refactor: 약관동의 페이지 구조 변경 및 footer 추가

--- src/pages/signup/Agreement.tsx
+++ src/pages/signup/Agreement.tsx
@@ -50,59 +50,66 @@ const Agreement = () => {
 
   return (
     <div className="agreement-container">
-      <ProgressBar />
-      <div className="agreement-title">
-        <Header className="Header" headerTitle="Flobby 회원가입을 시작합니다."/>
-        <span>
-          Flobby 회원이 되면 취미 모임과 커뮤니티를 통해 <br/> 
-          새로운 관심사를 발견하고 다양한 사람들과 교류할 수 있어요.
-        </span>
-      </div>
-
-      <main>
-        <div className="input-container">
-          <div className="all-wrapper">
-            <Checkbox
-              checked={allAgree}
-              onChange={handleAllAgree}
-              typename="약관 모두 동의"
-              withButton={false}
-            />
-          </div>
-          
-          <hr />   
-
-          <div className="rest-wrapper">
-            <Checkbox 
-              checked={serviceAgree}
-              onChange={handleServiceAgree}
-              typename="[필수] 서비스 이용약관 동의"
-              onClick={openServiceModal}
-            />
-
-            <Checkbox 
-              checked={privacyAgree}
-              onChange={handlePrivacyAgree}
-              typename="[필수] 개인정보 수집 및 이용 동의"
-              onClick={openPrivacyModal}
-            />
-
-            <Checkbox 
-              checked={marketingAgree}
-              onChange={handleMarketingAgree}
-              typename="[선택] 광고성 정보 수신 동의"
-              onClick={openMarketingModal}
-            />
-          </div>
+      <ProgressBar  />
+      <div className="agreement-wrapper">
+        <div className="agreement-title">
+          <Header className="Header" headerTitle="Flobby 회원가입을 시작합니다."/>
+          <span>
+            Flobby 회원이 되면 취미 모임과 커뮤니티를 통해 <br/> 
+            새로운 관심사를 발견하고 다양한 사람들과 교류할 수 있어요.
+          </span>
         </div>
 
-        <Button 
-          className={`join-next-btn ${serviceAgree && privacyAgree ? 'active' : ''}`} 
-          title="다음" onClick={acceptAgree}
-        />
+        <main>
+          <div className="input-container">
+            <div className="all-wrapper">
+              <Checkbox
+                checked={allAgree}
+                onChange={handleAllAgree}
+                typename="약관 모두 동의"
+                withButton={false}
+              />
+            </div>
+            
+            <hr />   
 
-        <AgreementModal onAgree={handleModalAgree}/>
-      </main>
+            <div className="rest-wrapper">
+              <Checkbox 
+                checked={serviceAgree}
+                onChange={handleServiceAgree}
+                typename="[필수] 서비스 이용약관 동의"
+                onClick={openServiceModal}
+              />
+
+              <Checkbox 
+                checked={privacyAgree}
+                onChange={handlePrivacyAgree}
+                typename="[필수] 개인정보 수집 및 이용 동의"
+                onClick={openPrivacyModal}
+              />
+
+              <Checkbox 
+                checked={marketingAgree}
+                onChange={handleMarketingAgree}
+                typename="[선택] 광고성 정보 수신 동의"
+                onClick={openMarketingModal}
+              />
+            </div>
+          </div>
+
+          <Button 
+            className={`join-next-btn ${serviceAgree && privacyAgree ? 'active' : ''}`} 
+            title="다음" onClick={acceptAgree}
+          />
+
+          <AgreementModal onAgree={handleModalAgree}/>
+        </main>
+
+      </div>
+      <footer>
+        <div className="agreement-line1"></div>
+        <div className="agreement-line2"></div>
+      </footer>
     </div>
   );
 };
