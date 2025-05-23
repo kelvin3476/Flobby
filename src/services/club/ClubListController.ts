05e4fc38e4de9ea040de08e86c8d948e4550923a
bluemin <zeem_m2@naver.com>
2025-05-23 22:24:26 +0900

    Feat: 모임 목록 컨트롤러 구현

---
+++ src/services/club/ClubListController.ts
@@ -0,0 +1,26 @@
+import { ClubListItem } from '../../api/ApiTypes';
+import { ClubItemsListModel } from './ClubListModel';
+
+export class ClubItemsListController {
+  private static instance: ClubItemsListController;
+  model: ClubItemsListModel;
+
+  private constructor() {
+    this.model = new ClubItemsListModel();
+  }
+
+  static getInstance(): ClubItemsListController {
+    if (
+      !ClubItemsListController.instance ||
+      ClubItemsListController.instance === null ||
+      ClubItemsListController.instance === undefined
+    ) {
+      ClubItemsListController.instance = new ClubItemsListController();
+    }
+    return ClubItemsListController.instance;
+  }
+
+  getClubList(category?: string): Promise<ClubListItem[]> {
+    return this.model.getClubList(category);
+  }
+}
