ecd59d854b46e165bc17e028c90fb74099fabc68
bluemin <zeem_m2@naver.com>
2025-05-27 18:00:40 +0900

    Feat: ClubAll 컴포넌트에 컴포넌트 추가

--- src/pages/club/ClubAll.tsx
+++ src/pages/club/ClubAll.tsx
@@ -1,8 +1,19 @@
 import React from 'react';
+import useClubCategoryStore from '../../store/club/useClubCategoryStore';
+import MainCategory from '../../components/club/list/MainCategory';
+import Title from '../../components/club/text/Title';
+import SubCategory from '../../components/club/list/SubCategory';
+import '../../styles/club/list/ClubMain.scss';
 
 const ClubAll = () => {
+  const { mainCategory } = useClubCategoryStore();
+
   return (
-   <></>
+    <>
+      <MainCategory />
+      <Title titleName={mainCategory ? mainCategory : '모임'} />
+      <SubCategory />
+    </>
   );
 };
 
