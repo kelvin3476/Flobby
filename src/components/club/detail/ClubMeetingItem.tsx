db671d97024d11f441d40b66b1b681b917744d9b
bluemin <zeem_m2@naver.com>
2025-06-03 10:31:23 +0900

    Refactor: 연동된 데이터에 맞게 타입 일치 및 props 수정

---
+++ src/components/club/detail/ClubMeetingItem.tsx
@@ -0,0 +1,169 @@
+import React from 'react';
+import Button from '../../button/Button';
+import '../../../styles/club/detail/ClubMeetingItem.scss';
+import { useNavigate } from 'react-router-dom';
+
+interface ClubMeetingProps {
+  meetingId: number;
+  loginMemberId: number; // clubDTO에 추가 될 예정
+  meetingLeaderId: number; // clubMeetingList에 추가 될 예정
+  clubMeetingTitle: string;
+  clubMeetingDate: string;
+  clubMeetingLocation: string;
+  maxParticipants: number;
+  currentParticipants: number;
+  isApplied: boolean;
+  isMember: boolean; // clubMeetingList에 추가 될 예정
+  role: string; // clubDTO에 추가 될 예정
+  entryfee: string;
+}
+const ClubMeetingItem = ({
+  meetingId,
+  loginMemberId,
+  meetingLeaderId,
+  clubMeetingTitle,
+  clubMeetingDate,
+  clubMeetingLocation,
+  maxParticipants,
+  currentParticipants,
+  isApplied,
+  isMember,
+  role,
+  entryfee,
+}: ClubMeetingProps) => {
+  const navigate = useNavigate();
+
+  const handleClickModifyButton = () => {
+    navigate(`/club/meeting/modify/${meetingId}`);
+  };
+
+  return (
+    <div className="club-meeting-item-container">
+      {/* 내용 영역 */}
+      <div className="club-meeting-item-content-box">
+        <div className="club-meeting-item-title-box">
+          <span>{clubMeetingTitle}</span>
+        </div>
+        <div className="club-meeting-item-info-box">
+          {/* 정기모임 일시 */}
+          <div className="club-meeting-date-box">
+            <div className="club-meeting-date-icon"></div>
+            <div className="club-meeting-date">
+              <span>{clubMeetingDate}</span>
+            </div>
+          </div>
+
+          <span className="club-meeting-item-divider-icon"></span>
+
+          {/* 정기모임 장소 */}
+          <div className="club-meeting-location-box">
+            <div className="club-meeting-location-icon"></div>
+            <div className="club-meeting-location">{clubMeetingLocation}</div>
+          </div>
+
+          <span className="club-meeting-item-divider-icon"></span>
+
+          {/* 정기모임 인원 */}
+          <div className="club-meeting-participant-box">
+            <div className="club-meeting-participant-icon"></div>
+            <div className="club-meeting-participant">
+              <div className="club-meeting-participant-num">
+                <span className="club-meeting-current-participant">
+                  {currentParticipants}
+                </span>
+                <span>/</span>
+                <span>{maxParticipants}</span>
+              </div>
+              <span>명</span>
+            </div>
+          </div>
+
+          <span className="club-meeting-item-divider-icon"></span>
+
+          {/* 참가비 */}
+          <div className="club-meeting-participationFee-box">
+            <div className="club-meeting-participationFee-icon"></div>
+            <div className="club-meeting-participationFee">
+              <span>{entryfee}</span>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      {/* 버튼 영역 */}
+      <div className="club-meeting-button-box">
+        {/* 모임 가입자만 버튼 제공, 미가입자는 버튼 제공 x */}
+        {isMember ? (
+          /* 모임장 & 운영진, 일반 사용자 두 가지 케이스로 분기 */
+          <>
+            {/* 모임장 & 운영진일 경우 정기 모임 수정/삭제 권한 O */}
+            {role === 'leader' || role === 'manager' ? (
+              <>
+                <Button
+                  type="button"
+                  className="club-meeting-button-modify"
+                  title="수정"
+                  onClick={handleClickModifyButton}
+                />
+                {/* 정기 모임 참석 여부 확인 */}
+                {isApplied ? (
+                  <Button
+                    type="button"
+                    className="club-meeting-button-cancel"
+                    title="취소"
+                    onClick={() => {}}
+                  />
+                ) : (
+                  <Button
+                    type="button"
+                    className="club-meeting-button-apply"
+                    title="참석"
+                    onClick={() => {}}
+                  />
+                )}
+              </>
+            ) : (
+              <>
+                {/* 일반 사용자의 경우 정기 모임 생성자 일 때, 생성자가 아닐 때로 분기*/}
+                {/* 정기 모임 생성자는 정기 모임 수정/삭제 권한 O, 참석이 default -> 취소 버튼 노출*/}
+                {loginMemberId === meetingLeaderId ? (
+                  <>
+                    <Button
+                      type="button"
+                      className="club-meeting-button-modify"
+                      title="수정"
+                      onClick={handleClickModifyButton}
+                    />
+                    <Button
+                      type="button"
+                      className="club-meeting-button-cancel"
+                      title="취소"
+                      onClick={() => {}}
+                    />
+                  </>
+                ) : /* 정기 모임 생성자가 아닐 경우 수정/삭제 권한 X, 정기 모임 참석 여부만 확인 */
+                isApplied ? (
+                  <Button
+                    type="button"
+                    className="club-meeting-button-cancel"
+                    title="취소"
+                    onClick={() => {}}
+                  />
+                ) : (
+                  <Button
+                    type="button"
+                    className="club-meeting-button-apply"
+                    title="참석"
+                    onClick={() => {}}
+                  />
+                )}
+              </>
+            )}
+          </>
+        ) : null}
+      </div>
+    </div>
+  );
+};
+
+export default ClubMeetingItem;
