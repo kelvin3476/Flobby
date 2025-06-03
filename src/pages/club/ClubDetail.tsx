295f627606050a5f32c72dc3161e7279331d859a
bluemin <zeem_m2@naver.com>
2025-06-03 10:29:20 +0900

    Refactor: 정기 모임 데이터 연동

--- src/pages/club/ClubDetail.tsx
+++ src/pages/club/ClubDetail.tsx
@@ -1,14 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useParams } from 'react-router-dom';
 
 import { ClubController } from '../../services/club/controllers/ClubController';
 
 import logger from '../../utils/Logger';
-import ClubMeetingList from '../../components/club/club_meeting/ClubMeetingList';
-import { ClubMeetingListData } from '../../components/club/club_meeting/ClubMeetingListData'; // TODO: 정기 모임 연동 테스트 하면서 삭제 예정
+import ClubMeetingList from '../../components/club/detail/ClubMeetingList';
+import { ClubMeetingListItem } from '../../api/ApiTypes';
 
 const ClubDetail = () => {
   const { clubId } = useParams<{ clubId: string }>();
+  const [clubMeetingList, setClubMeetingList] = useState<ClubMeetingListItem[]>(
+    [],
+  );
 
   React.useEffect(() => {
     if (!clubId) {
@@ -24,6 +27,7 @@ const ClubDetail = () => {
         const response = await ClubController.getInstance().selectClubDetail(
           Number(clubId),
         );
+        setClubMeetingList(response.clubMeetingList);
         logger.log('모임 상세 정보:', response);
       } catch (error) {
         logger.error('모임 상세 정보를 가져오는 중 오류 발생:', error);
@@ -33,17 +37,13 @@ const ClubDetail = () => {
     fetchClubDetail();
   }, [clubId]);
 
-  // TODO: 추후 연동 테스트하면서 삭제 예정
-  // 정기모임용 테스트 데이터
-  const clubMeetingList = ClubMeetingListData;
-
   return (
     <div>
       <ClubMeetingList
         clubMeetingList={clubMeetingList}
-        loginMemberId={12}
-        role="leader"
-        isMember={true}
+        loginMemberId={12} //TODO: 데이터 연동 필요
+        role="leader" //TODO: 데이터 연동 필요
+        isMember={true} //TODO: 데이터 연동 필요
       />
     </div>
   );
