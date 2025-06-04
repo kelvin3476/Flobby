1762bfe2657c08624898acae18e5f942dcf319f3
bluemin <zeem_m2@naver.com>
2025-06-04 12:45:08 +0900

    Refactor: 메인 헤더의 모임 목록 페이지로 이동하는 경로 변경된 주소로 수정(club/all -> club/list)

--- src/components/header/MainHeader.tsx
+++ src/components/header/MainHeader.tsx
@@ -1,17 +1,17 @@
-import React, { useState } from "react";
-import { useNavigate } from "react-router-dom";
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
-import Logo from "../logo/Logo";
-import RegionSelector from "../main/region_selector/RegionSelector";
-import TextButton from "../button/TextButton";
-import SearchBar from "../main/search_bar/SearchBar";
-import Button from "../button/Button";
-import DropDownModal from "../modal/DropDownModal";
+import Logo from '../logo/Logo';
+import RegionSelector from '../main/region_selector/RegionSelector';
+import TextButton from '../button/TextButton';
+import SearchBar from '../main/search_bar/SearchBar';
+import Button from '../button/Button';
+import DropDownModal from '../modal/DropDownModal';
 
-import "../../styles/header/MainHeader.scss";
+import '../../styles/header/MainHeader.scss';
 
 const MainHeader = ({ accessToken }: { accessToken: string | null }) => {
-  const [ isClicked, setIsClicked ] = useState(false);
+  const [isClicked, setIsClicked] = useState(false);
   const nav = useNavigate();
   const hasAccessToken = !!accessToken;
 
@@ -21,39 +21,68 @@ const MainHeader = ({ accessToken }: { accessToken: string | null }) => {
         <div className="main-wrapper">
           <div className="up-wrapper">
             <div className="left-wrapper">
-              <Logo className="header-logo" onClick={() => nav('/')} />  
-              <RegionSelector /> 
+              <Logo className="header-logo" onClick={() => nav('/')} />
+              <RegionSelector />
             </div>
             <div className="right-wrapper">
               <SearchBar />
-              <div className={`btns-wrapper ${hasAccessToken ? "icons" : ""}`}>
+              <div className={`btns-wrapper ${hasAccessToken ? 'icons' : ''}`}>
                 {!hasAccessToken ? (
                   <>
-                    <Button type="button" className="login-btn" title="로그인" onClick={() => nav("/login")}/>
-                    <Button type="button" className="signup-btn" title="회원가입" onClick={() => nav("/signup/agreement")}/>
+                    <Button
+                      type="button"
+                      className="login-btn"
+                      title="로그인"
+                      onClick={() => nav('/login')}
+                    />
+                    <Button
+                      type="button"
+                      className="signup-btn"
+                      title="회원가입"
+                      onClick={() => nav('/signup/agreement')}
+                    />
                   </>
-                ): (
+                ) : (
                   <>
                     <button className="notice-btn"></button>
                     <button className="saved-btn"></button>
-                    <div 
-                      className={`profile-wrapper ${isClicked ? "clicked" : ""}`}
+                    <div
+                      className={`profile-wrapper ${isClicked ? 'clicked' : ''}`}
                       onClick={() => setIsClicked(prev => !prev)}
                     >
-                      <img 
+                      <img
                         className="profile-btn"
-                        src="../../img/header/profile-ex.jpg" />
+                        src="../../img/header/profile-ex.jpg"
+                      />
                     </div>
                   </>
                 )}
               </div>
             </div>
           </div>
-          {isClicked && <DropDownModal className="profile-modal" firstTitle="마이페이지" secondTitle="로그아웃" />}
+          {isClicked && (
+            <DropDownModal
+              className="profile-modal"
+              firstTitle="마이페이지"
+              secondTitle="로그아웃"
+            />
+          )}
           <div className="down-wrapper">
-            <TextButton className="club-btn" buttonName="모임" onClick={() => nav("/club/all", { state: accessToken })}/>
-            <TextButton className="oneday-btn" buttonName="원데이" onClick={() => nav("/oneday/all")}/>
-            <TextButton className="community-btn" buttonName="정보공유" onClick={() => nav("/community/all")}/>
+            <TextButton
+              className="club-btn"
+              buttonName="모임"
+              onClick={() => nav('/club/list', { state: accessToken })}
+            />
+            <TextButton
+              className="oneday-btn"
+              buttonName="원데이"
+              onClick={() => nav('/oneday/all')}
+            />
+            <TextButton
+              className="community-btn"
+              buttonName="정보공유"
+              onClick={() => nav('/community/all')}
+            />
           </div>
         </div>
       </div>
