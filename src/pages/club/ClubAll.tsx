9cdb593b89bf1daf5c6a593b5a92076eb4802dc1
bluemin <zeem_m2@naver.com>
2025-06-06 18:29:10 +0900

    Refactor: 지역 변경에 따라 모임 목록 업데이트

--- src/pages/club/ClubAll.tsx
+++ src/pages/club/ClubAll.tsx
@@ -7,16 +7,18 @@ import Title from '../../components/club/text/Title';
 import SubCategory from '../../components/club/list/SubCategory';
 import ClubList from '../../components/club/list/ClubList';
 import FloatingButton from '../../components/button/FloatingButton';
-import useMainPage from "../../hooks/main/useMainPage";
+import useMainPage from '../../hooks/main/useMainPage';
 import useClubCategoryStore from '../../store/club/useClubCategoryStore';
 import { CategoryListController } from '../../services/category/controllers/CategoryListController';
 import { ClubController } from '../../services/club/controllers/ClubController';
 import { ClubListItem, HobbyCategory } from '../../api/ApiTypes';
-import FabDefaultIcon from "../../assets/svg/club/clublist/floating_button_default.svg";
-import FabDefaultCancelIcon from "../../assets/svg/club/clublist/floating_button_default_cancel.svg";
-import FabClubRegisterIcon from "../../assets/svg/club/clublist/floating_button_club_register.svg";
-import FabOnedayRegisterIcon from "../../assets/svg/club/clublist/floating_button_oneday_register.svg";
+import FabDefaultIcon from '../../assets/svg/club/clublist/floating_button_default.svg';
+import FabDefaultCancelIcon from '../../assets/svg/club/clublist/floating_button_default_cancel.svg';
+import FabClubRegisterIcon from '../../assets/svg/club/clublist/floating_button_club_register.svg';
+import FabOnedayRegisterIcon from '../../assets/svg/club/clublist/floating_button_oneday_register.svg';
 import '../../styles/main/club/ClubAll.scss';
+import { getCookie } from '../../utils/Cookie';
+import { RegionContextController } from '../../services/region/controllers/RegionContextController';
 
 const ClubAll = () => {
   const navigate = useNavigate();
@@ -27,6 +29,8 @@ const ClubAll = () => {
   const { mainCategory, subCategory } = useClubCategoryStore();
   const [categoryList, setCategoryList] = useState<HobbyCategory[]>([]);
   const [clubList, setClubList] = useState<ClubListItem[]>([]);
+
+  const [selectedRegion, setSelectedRegion] = useState<string>('');
 
   // 카테고리 목록 컨트롤러
   const categoryListController = CategoryListController.getInstance();
@@ -52,30 +56,47 @@ const ClubAll = () => {
   // 모임 목록 컨트롤러
   const clubController = ClubController.getInstance();
 
-  useEffect(() => {
-    const fetchClubItemsList = async () => {
-      if (!mainCategory || mainCategory === '전체') {
-        const clubListData = await clubController.getClubList();
-        setClubList(clubListData);
-      } else {
-        const clubListData = await clubController.getClubList(
-          encodeURIComponent(mainCategory),
-        );
-        setClubList(clubListData);
-      }
-    };
+  const fetchClubItemsList = async () => {
+    if (!mainCategory || mainCategory === '전체') {
+      const clubListData = await clubController.getClubList();
+      setClubList(clubListData);
+    } else {
+      const clubListData = await clubController.getClubList(
+        encodeURIComponent(mainCategory),
+      );
+      setClubList(clubListData);
+    }
+  };
 
+  // 카테고리 값 변경 시 모임 목록 업데이트
+  useEffect(() => {
     fetchClubItemsList();
   }, [mainCategory, subCategory]);
 
+  // 지역 변경 시 모임 목록 업데이트
+  useEffect(() => {
+    const handleRegionChange = () => {
+      fetchClubItemsList();
+    };
+
+    window.addEventListener('regionChanged', handleRegionChange);
+    return () => {
+      window.removeEventListener('regionChanged', handleRegionChange);
+    };
+  }, []);
+
   return (
     <div className="club-all-wrapper">
-      <MainHeader accessToken={accessToken} mainDataList={mainDataList} setMainDataList={setMainDataList} />
-      <div className='club-all-content-container'>
-        <MainCategory categoryList={categoryList} />  
-        <div className='club-all-content'>
+      <MainHeader
+        accessToken={accessToken}
+        mainDataList={mainDataList}
+        setMainDataList={setMainDataList}
+      />
+      <div className="club-all-content-container">
+        <MainCategory categoryList={categoryList} />
+        <div className="club-all-content">
           <Title titleName={mainCategory ? mainCategory : '모임'} />
-          <div className='club-all-sub-content'>  
+          <div className="club-all-sub-content">
             <SubCategory categoryList={categoryList} />
             <ClubList clubList={clubList} />
           </div>
@@ -86,8 +107,16 @@ const ClubAll = () => {
           mainDefaultIcon={FabDefaultIcon}
           mainActionIcon={FabDefaultCancelIcon}
           options={[
-            { icon: FabClubRegisterIcon, label: '모임 등록', onClick: () => navigate('/club/register') },
-            { icon: FabOnedayRegisterIcon, label: '원데이 등록', onClick: () => navigate('/oneday/register')},
+            {
+              icon: FabClubRegisterIcon,
+              label: '모임 등록',
+              onClick: () => navigate('/club/register'),
+            },
+            {
+              icon: FabOnedayRegisterIcon,
+              label: '원데이 등록',
+              onClick: () => navigate('/oneday/register'),
+            },
           ]}
         />
       )}
