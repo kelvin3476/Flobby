a62b2e32fe927dcfd94a9257f6e8db069cca3744
bluemin <zeem_m2@naver.com>
2025-06-10 16:10:03 +0900

    Feat: 정기모임명 컨포넌트 구현

---
+++ src/components/club/meetingRegister/ClubMeetingTitle.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import Label from '../register/Label';
+import useClubMeetingRegisterStore from '../../../store/club/useClubMeetingRegisterStore';
+
+const ClubMeetingTitle = () => {
+  const {
+    clubMeetingTitle,
+    setClubMeetingTitle,
+    isClubMeetingTitleValid,
+    setIsClubMeetingTitleValid,
+    clubMeetingTitleError,
+    setClubMeetingTitleError,
+  } = useClubMeetingRegisterStore();
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const value = e.target.value;
+    setClubMeetingTitle(value);
+
+    if (value.trim() === '') {
+      setIsClubMeetingTitleValid(false);
+      setClubMeetingTitleError('제목을 입력해 주세요.');
+    } else {
+      setIsClubMeetingTitleValid(true);
+      setClubMeetingTitleError('');
+    }
+  };
+
+  return (
+    <div className="club-meeting-content-container">
+      <Label labelTitle="정기 모임명" />
+      <input
+        type="text"
+        placeholder="모임명을 입력해 주세요."
+        value={clubMeetingTitle}
+        onChange={handleChange}
+      />
+      {!isClubMeetingTitleValid && (
+        <div className="error">{clubMeetingTitleError}</div>
+      )}
+    </div>
+  );
+};
+
+export default ClubMeetingTitle;
