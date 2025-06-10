188b69ba8512b47b9cdc744f6ced342c28b6541e
bluemin <zeem_m2@naver.com>
2025-06-10 12:47:35 +0900

    Design: 데이트 피커 연동

--- src/pages/club/ClubMeetingRegister.tsx
+++ src/pages/club/ClubMeetingRegister.tsx
@@ -1,7 +1,31 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
+import DatePicker from '../../utils/DatePicker';
+
+import '../../styles/datepicker/DatePicker.scss';
 
 const ClubMeetingRegister = () => {
-  return <div>정기 모임 등록 페이지</div>;
+  const datepickerRef = useRef<HTMLDivElement>(null);
+  const triggerRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    if (datepickerRef.current) {
+      new DatePicker(datepickerRef.current, {
+        trigger: triggerRef.current,
+        onSelect: date => {
+          console.log(date);
+          /* TODO: 선택된 날짜를 데이터 형식에 맞게 가공 후 전역에 저장 */
+        },
+      });
+    }
+  }, []);
+
+  return (
+    <div>
+      {/* TODO: 날짜 선택 => 퍼블 필요 */}
+      <div ref={triggerRef}>날짜 선택</div>
+      <div className="datepicker-container" ref={datepickerRef} />
+    </div>
+  );
 };
 
 export default ClubMeetingRegister;
