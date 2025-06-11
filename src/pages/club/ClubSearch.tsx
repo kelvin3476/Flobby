9b3dc6fea07e596f8126a101079b908130e1434c
kelvin3476 <seungu.lee@g.austincc.edu>
2025-06-11 16:54:23 +0900

    Design: 모임 검색 페이지 추가

---
+++ src/pages/club/ClubSearch.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const ClubSearch = () => {
+
+  return (
+    <div className='club-search-wrapper'>
+      <div>모임 검색 페이지</div>
+    </div>
+  );
+};
+
+export default ClubSearch;
