d2b0089b05f99e49ad5cd06de3019c66c4873612
ongzzzz <kyjj0506@naver.com>
2025-06-13 10:07:52 +0900

    Refactor: 불필요한 코드 삭제

--- src/api/main/Main.ts
+++ src/api/main/Main.ts
@@ -35,10 +35,6 @@ export default class Main {
 
   /* 정기 모임 생성 */
   static async createClubMeeting(createClubMeetingData: CreateClubMeetingData, clubId: number) {
-    return await http.post(`/club/${clubId}/clubmeeting/register`, createClubMeetingData, {
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    });
+    return await http.post(`/club/${clubId}/clubmeeting/register`, createClubMeetingData);
   }
 }
