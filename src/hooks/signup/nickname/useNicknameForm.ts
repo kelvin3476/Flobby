e3cd36e6bcff233a1317cac484d732820871b01d
ongzzzz <kyjj0506@naver.com>
2025-06-18 17:59:26 +0900

    Fix: 닉네임 한글 추가 + 실시간 유효성 검사

--- src/hooks/signup/nickname/useNicknameForm.ts
+++ src/hooks/signup/nickname/useNicknameForm.ts
@@ -13,19 +13,19 @@ const useNicknameForm = () => {
   } = useNicknameStore();
 
   const isValidNickname = (nickname: string) => {
-    const nicknameRegex = /^(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]{2,12}$/;
+    const nicknameRegex = /^(?=.*[a-zA-Z가-힣])(?=.*\d)[a-zA-Z가-힣\d]{2,12}$/;
 
     return nicknameRegex.test(nickname);
   };
 
-  //닉네임 입력> 유효성검사 > 문자숫자 만족시 중복확인 버튼 활성화 > 중복확인 결과 체크 > 사용가능한 닉네임입니다.
-  const handleNicknameBlur = () => {
+  const handleNicknameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const nickname = e.target.value;
+    setNickname(nickname);
+
     if (!isValidNickname(nickname)) {
       setIsNicknameValid(false);
       setNicknameError(['warning', '문자+숫자 조합 2~12자리를 입력해 주세요.']);
-      return;
     } else {
-      //중복확인 버튼 활성화
       setIsNicknameValid(true);
       setNicknameError(['default', '문자+숫자 조합 2~12자리']);
     }
@@ -47,10 +47,6 @@ const useNicknameForm = () => {
     }
   };
 
-  const handleNicknameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setNickname(e.target.value);
-  };
-
   return {
     nickname,
     setNickname,
@@ -58,7 +54,6 @@ const useNicknameForm = () => {
     setIsNicknameValid,
     nicknameError,
     setNicknameError,
-    handleNicknameBlur,
     handleNicknameChange,
     checkDuplicatedNickname,
   };
