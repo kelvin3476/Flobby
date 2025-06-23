a8ea88b9f8ceb92f755c1e8398a34557ba2deb65
ongzzzz <kyjj0506@naver.com>
2025-06-23 12:26:15 +0900

    Design: Logo 만 있는 헤더 컴포넌트 생성

---
+++ src/components/header/LogoHeader.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+
+import Logo from "../logo/Logo";
+
+import "../../styles/header/LogoHeader.scss";
+
+const LogoHeader = () => {
+  const nav = useNavigate();
+
+  return (
+    <div className="logo-header-container">
+      <div className="logo-header-wrapper">
+        <Logo className="logo-logo" onClick={() => nav('/')}/>  
+      </div>
+    </div>
+  );
+};
+
+export default LogoHeader;
