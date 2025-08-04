148ef5c075d6a44572ce320d2687044168f37a49
kelvin3476 <seungu.lee@g.austincc.edu>
2025-08-04 13:37:26 +0900

    Refactor: 페이지 트랙킹 커스텀 훅 내부 RouteConfig 에서 관리 하는 리스트로 매칭 라우트를 찾아서 해당 라우트에 title 값 을 GA4 로 전송 하는 로직 추가

--- src/hooks/gtag/useGtagPageTracking.ts
+++ src/hooks/gtag/useGtagPageTracking.ts
@@ -1,15 +1,37 @@
 // src/hooks/useGtagPageTracking.ts
 import { useEffect } from 'react';
-import { useLocation } from 'react-router-dom';
+import { useLocation, matchPath } from 'react-router-dom';
+import { RouteConfig } from '../../routes/RouteConfig';
 
 export function useGtagPageTracking() {
   const location = useLocation();
 
   useEffect(() => {
-    if (typeof window.gtag === 'function') {
-      window.gtag('config', 'G-3094XR81MN', {
-        page_path: location.pathname + location.search,
-        page_location: window.location.href, // ✅ 이걸 명시적으로 지정하면 dl 값도 갱신됨
+    const matchedRoute = RouteConfig.find((route) =>
+      matchPath({ path: route.path, end: true }, location.pathname)
+    );
+
+    const baseTitle = 'Flobby';
+
+    const title = matchedRoute?.title
+      ? `${matchedRoute.title} | ${baseTitle}`
+      : baseTitle;
+
+    // document.title = title; // seo 최적화 시 권장 되는 라우팅 되는 페이지 별 탭 이름 적용 => ux 적인 측면 보안 (강력 권장)
+
+    // if (typeof window.gtag === 'function') {
+    //   window.gtag('config', 'G-3094XR81MN', {
+    //     page_path: location.pathname + location.search,
+    //     page_location: window.location.href, // ✅ 이걸 명시적으로 지정하면 dl 값도 갱신됨
+    //     page_title: title,
+    //   });
+    // }
+
+    if (window.gtag) {
+      window.gtag('event', 'page_view', {
+        page_path: decodeURI(location.pathname + location.search),
+        page_location: decodeURI(window.location.href), // ✅ 이걸 명시적으로 지정하면 dl 값도 갱신됨
+        page_title: decodeURI(title),
       });
     }
   }, [location]);
