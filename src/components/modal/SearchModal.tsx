9feee48da9c364004d19c41af6634e86a90e7d07
bluemin <zeem_m2@naver.com>
2025-09-23 00:30:05 +0900

    Remove: 불필요한 import 삭제

--- src/components/modal/SearchModal.tsx
+++ src/components/modal/SearchModal.tsx
@@ -3,7 +3,6 @@ import React from 'react';
 import Chip from '@/components/button/Chip';
 
 import '@/styles/modal/SearchModal.scss';
-import { SearchChallengeController } from '@/services/challenge/controllers/SearchChallengeController';
 import { PopularKeywordData } from '@/api/ApiTypes';
 import { useNavigate } from 'react-router-dom';
 import useSearchKeywordStore from '@/store/main/useSearchKeywordStore';
