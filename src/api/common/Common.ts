2075612c0c69b37a14fcfb71a6cd00279993d4c5
kelvin3476 <seungu.lee@g.austincc.edu>
2025-10-22 11:10:04 +0900

    Refactor: 헤더 조회 api return 타입 추가

--- src/api/common/Common.ts
+++ src/api/common/Common.ts
@@ -1,8 +1,9 @@
 import { http } from '@/utils/Http';
+import { GetHeaderInfoResponse } from '@/api/ApiTypes';
 
 export default class Common {
   /* 헤더 조회 */
-  static async getHeaderInfo() {
+  static async getHeaderInfo(): GetHeaderInfoResponse {
     return await http.get('/header');
   }
 }
