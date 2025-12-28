00b5e9a51eda2d884a5da3e5e9b90c0c88c99907
bluemin <zeem_m2@naver.com>
2025-12-28 17:25:28 +0900

    Refactor: 리뷰 없을 때 예외처리

--- src/components/challenge/detail/ChallengeDetailReview.tsx
+++ src/components/challenge/detail/ChallengeDetailReview.tsx
@@ -23,29 +23,41 @@ const ChallengeDetailReview = ({
 
       <div className="challenge-detail-review-content-wrapper">
         <div className="challenge-detail-review-content-container">
-          {challengeDetailReview?.map((challengeDetailReviewItem, index) => (
-            <>
-              {index !== 0 && (
-                <div className="challenge-detail-review-item-divider"></div>
-              )}
-              <ChallengeDetailReviewItem
-                ChallengeDetailReviewItem={challengeDetailReviewItem}
-              />
-            </>
-          ))}
+          {challengeDetailReview &&
+            challengeDetailReview.length > 0 &&
+            challengeDetailReview?.map((challengeDetailReviewItem, index) => (
+              <>
+                {index !== 0 && (
+                  <div className="challenge-detail-review-item-divider"></div>
+                )}
+                <ChallengeDetailReviewItem
+                  ChallengeDetailReviewItem={challengeDetailReviewItem}
+                />
+              </>
+            ))}
+
+          {/* TODO: 임치예외처리 => 메세지 확인 필요 */}
+          {challengeDetailReview.length === 0 && (
+            <div className="challenge-detail-review-empty">
+              아직 작성된 후기가 없어요
+            </div>
+          )}
         </div>
 
-        <div className="challenge-detail-review-btn-container">
-          <div
-            className="challenge-detail-review-more-btn"
-            onClick={() => navigate(`/challenge/${challengeId}/review`)}
-          >
-            <div className="challenge-detail-review-more-btn-text">
-              {challengeDetailReview?.length}개 후기 전체보기
+        {/* TODO: 후기 없을 때 버튼 노출 X => 예외처리 확인 필요 */}
+        {challengeDetailReview && challengeDetailReview.length > 0 && (
+          <div className="challenge-detail-review-btn-container">
+            <div
+              className="challenge-detail-review-more-btn"
+              onClick={() => navigate(`/challenge/${challengeId}/review`)}
+            >
+              <div className="challenge-detail-review-more-btn-text">
+                {challengeDetailReview.length}개 후기 전체보기
+              </div>
+              <div className="challenge-detail-review-more-btn-icon"></div>
             </div>
-            <div className="challenge-detail-review-more-btn-icon"></div>
           </div>
-        </div>
+        )}
       </div>
     </div>
   );
