131f534016e6e920afa53aeba724877c18ae98be
kelvin3476 <winrain03226@gmail.com>
2026-01-01 22:04:34 +0900

    Refactor: 챌린지 상세 > 설명 영역 컴포넌트 더보기 버튼 추가

--- src/components/challenge/detail/ChallengeRecruitDescription.tsx
+++ src/components/challenge/detail/ChallengeRecruitDescription.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 
 import { ChallengeRecruitDescriptionType } from '@/api/ApiTypes';
 
@@ -8,21 +8,52 @@ interface ChallengeRecruitDescriptionTypeProps {
   challengeRecruitDescriptionType: ChallengeRecruitDescriptionType;
 }
 
+const MAX_HEIGHT = 1200;
+
 const ChallengeRecruitDescription = ({ challengeRecruitDescriptionType }: ChallengeRecruitDescriptionTypeProps) => {
+  const contentRef = useRef<HTMLDivElement>(null);
+  const [isExpanded, setIsExpanded] = useState(false);
+  const [showMoreButton, setShowMoreButton] = useState(false);
+
+  useEffect(() => {
+    if (contentRef.current) {
+      const height = contentRef.current.scrollHeight;
+      if (height > MAX_HEIGHT) {
+        setShowMoreButton(true);
+      }
+    }
+  }, [challengeRecruitDescriptionType]);
 
   return (
     <div className="challenge-recruit-description-wrapper">
       <div className="challenge-recruit-description-title">우리 챌린지를 소개해요</div>
-      <div className="challenge-recruit-description">{ challengeRecruitDescriptionType?.description }</div>
-      {challengeRecruitDescriptionType &&
-      challengeRecruitDescriptionType.images &&
-      challengeRecruitDescriptionType.images.length > 0 &&
-      <div className="challenge-recruit-description-images">
-        {challengeRecruitDescriptionType?.images.map((challengeRecruitDescriptionTypeItem, index) => (
-            <img key={challengeRecruitDescriptionTypeItem.orderNo} src={challengeRecruitDescriptionTypeItem.imageUrl} alt="챌린지 소개글 이미지" />
-        ))}
+      <div
+        ref={contentRef}
+        className={`challenge-recruit-description-content ${
+          isExpanded ? 'expanded' : ''
+        }`}
+      >
+        <div className="challenge-recruit-description">{ challengeRecruitDescriptionType?.description }</div>
+        {challengeRecruitDescriptionType &&
+        challengeRecruitDescriptionType.images &&
+        challengeRecruitDescriptionType.images.length > 0 &&
+        <div className="challenge-recruit-description-images">
+          {challengeRecruitDescriptionType?.images.map((challengeRecruitDescriptionTypeItem, index) => (
+              <img key={challengeRecruitDescriptionTypeItem.orderNo} src={challengeRecruitDescriptionTypeItem.imageUrl} alt="챌린지 소개글 이미지" />
+          ))}
+        </div>
+        }
       </div>
-      }
+
+      {showMoreButton && (
+        <button
+          className="challenge-recruit-description-more-btn"
+          onClick={() => setIsExpanded((prev) => !prev)}
+        >
+          {isExpanded ? '접기' : '더보기'}
+          <span className="challenge-recruit-description-more-btn-image"></span>
+        </button>
+      )}
     </div>
   );
 };
