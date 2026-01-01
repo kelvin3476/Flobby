3cf40ffab09e0aa1a52911275691f2908a476739
kelvin3476 <winrain03226@gmail.com>
2026-01-01 16:54:43 +0900

    Feat: 공통 디바이더 컴포넌트 추가

---
+++ src/components/divider/Divider.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import '@/styles/divider/divider.scss';
+
+const Divider = () => {
+  return (
+    <div className="divider"></div>
+  );
+};
+
+export default Divider;
